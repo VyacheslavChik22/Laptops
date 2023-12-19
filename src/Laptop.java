83b3daab7394bf560b3e9033994c8b52cbe81f65
Vyacheslav_Zyablov <asyofka@mail.ru>
2023-12-19 11:25:16 +0300

    Add laptops

---
+++ src/Laptop.java
@@ -0,0 +1,50 @@
+public class Laptop {
+    private String name;
+    private int hard;
+    private int ram;
+    private String os;
+    private String color;
+    private int id;
+    private static int count = 1;
+
+    public Laptop(String name,int hard, int ram, String os, String color) {
+        this.name = name;
+        this.hard = hard;
+        this.ram = ram;
+        this.os = os;
+        this.color = color;
+        this.id = getCount(count);
+        count++;
+    }
+
+    public static int getCount(int count) {
+        return count;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getHard() {
+        return hard;
+    }
+    public int getRam() {
+        return ram;
+    }
+    public String getOs() {
+        return os;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "Ноутбук" +
+                "id" + id +
+                " Модель: " + name +
+                ", жесткий диск: " + hard + " GB " +
+                ", оперативная память: " + ram + " GB " +
+                ", оперативная система: " + os +
+                ", цвет модели: " + color;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Laptop> laptops = new HashSet<>();
+        laptops.add(new Laptop("HP",256,8,"Windows 10","Silver"));
+        laptops.add(new Laptop("Lenovo",512,64,"MacOS","Black"));
+        laptops.add(new Laptop("Asus",512,32,"Windows 11","White"));
+        laptops.add(new Laptop("Dell",256,16,"Linux","Silver"));
+        laptops.add(new Laptop("Huawei",128,4,"Windows 10","Blue"));
+        Map<Integer, String> criterion = new HashMap<>();
+        criterion.put(1,"Hard");
+        criterion.put(1,"Ram");
+        criterion.put(1,"Operation System");
+        criterion.put(1,"Color");
+    }
 }
